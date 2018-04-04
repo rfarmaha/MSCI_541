37f9a9c49353ebbf75807b1cac6925c3dcf8f8e8
rfarmaha <ramandeep.f@gmail.com>
2018-04-03 23:30:44 -0400

    Made search changes

--- A5/search.py
+++ A5/search.py
@@ -1,5 +1,5 @@
 """
-File: evaluate.py
+File: search.py
 Author: Ramandeep Farmaha 20516974
 Date Last Modified: April 2nd, 2018
 Python Version: 3.4
@@ -9,7 +9,9 @@ Submits query to document engine that performs BM25 retrieval and displays top 1
 import argparse
 import bm25
 import getDocument
+import queryBiasedSummary
 import pickle
+import time
 
 DIRECTORY_PATH = "../documents/"
 TOKEN_ID_TOKEN_PATH = 'token_id_token.p'
@@ -18,17 +20,6 @@ TOKEN_ID_POSTINGS_PATH = 'token_id_postings.p'
 DOC_ID_NO_PATH = '/doc_id_no.p'
 TOPICS_PATH = '../topics.p'
 RESULTS_PATH = 'bm25-results/'
-
-# def parse_args():
-#     parser = argparse.ArgumentParser(description='Program that displays top 10 ranked results for a given query')
-#     parser.add_argument('--qrel', required=True, help='Path to qrel file')
-#     parser.add_argument('--output_directory', required=True, help='Path to desired output directory')
-#     parser.add_argument('--results', required=False, help='Path to directory containing results files')
-#     parser.add_argument('--results_files', required=False, nargs='+', help='Space delimited list of results files')
-#     parser.add_argument('--compare', required=False, nargs=2, help='Outputs topics for which output_file_1 performs '
-#                                                                    'better than output_file_1')
-#     args = parser.parse_args()
-#     return args.qrel, args.output_directory, args.results, args.results_files, args.compare
 
 
 # Import relevant dicts of tokens to postings
@@ -45,14 +36,21 @@ print("Calculating average length of documents")
 average_length = 524.3251273730818
 print(average_length)
 query = input("Enter a query: ")
+start = time.time()
 
 o_dict = bm25.calculate_bm25(0, query, token_token_id, token_id_postings, doc_id_no, average_length, False, DIRECTORY_PATH)
-# Display top ten results
-limit = min(len(o_dict), 10)
-i = 1
-for doc_no, score in o_dict.items():
-    document = getDocument.retrieve_by_docno(DIRECTORY_PATH, doc_no)
-    print("{}. {} ({})".format(i, document.headline.replace('\n', ''), document.date))
-    i += 1
-    if i > limit:
-        break
+# # Display top ten results
+# limit = min(len(o_dict), 10)
+# i = 1
+# for doc_no, score in o_dict.items():
+#     document = getDocument.retrieve_by_docno(DIRECTORY_PATH, doc_no)
+#     print("{}. {} ({})".format(i, document.headline.replace('\n', ''), document.date))
+#     print(document.text)
+#     queryBiasedSummary.summarize(query, document.text)
+#     break
+#     i += 1
+#     if i > limit:
+#         break
+
+stop = time.time()
+print(stop - start)
