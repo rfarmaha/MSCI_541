91ec963ec4b23dd0e0652a0af13e1652f9587793
rfarmaha <ramandeep.f@gmail.com>
2018-04-04 01:54:01 -0400

    Added interactive shell

--- A5/search.py
+++ A5/search.py
@@ -22,37 +22,78 @@ DOC_ID_NO_PATH = '/doc_id_no.p'
 TOPICS_PATH = '../topics.p'
 RESULTS_PATH = 'bm25-results/'
 
+VALID_INPUTS = ['N', 'Q']
 
-# Import relevant dicts of tokens to postings
-print("Importing relevant dicts")
-token_id_postings = pickle.load(open(DIRECTORY_PATH + TOKEN_ID_POSTINGS_PATH, 'rb'))
-token_id_token = pickle.load(open(DIRECTORY_PATH + TOKEN_ID_TOKEN_PATH, 'rb'))
-token_token_id = pickle.load(open(DIRECTORY_PATH + TOKEN_TOKEN_ID_PATH, 'rb'))
-doc_id_no = pickle.load(open(DIRECTORY_PATH + DOC_ID_NO_PATH, 'rb'))
-doc_no_metadata = pickle.load(open(DIRECTORY_PATH + DOC_NO_METADATA_PATH, 'rb'))
-print("Completed import")
 
-# Calculate average doc length in collection
-print("Calculating average length of documents")
-average_length = sum([doc_no_metadata[doc_no].length for doc_id, doc_no in doc_id_no.items()]) / len(doc_id_no)
+def handle_query(query, token_token_id, token_id_postings, doc_id_no, doc_no_metadata, average_length, DIRECTORY_PATH):
+    """Returns dictionary of document rank to docno"""
+    start = time.time()
+    rank_doc_no = {}
 
-query = input("Enter a query: ")
-start = time.time()
+    o_dict = bm25.calculate_bm25(0, query, token_token_id, token_id_postings, doc_id_no, doc_no_metadata,
+                                 average_length, False, DIRECTORY_PATH)
+    # Display top ten results
+    limit = min(len(o_dict), 10)
+    i = 1
+    for doc_no, score in o_dict.items():
+        document = doc_no_metadata[doc_no]
+        summary = queryBiasedSummary.summarize(query, document.text)
+        if not document.headline:
+            document.headline = "{}...".format(summary[:50])
+        print("{}. {}({})".format(i, document.headline.replace('\n', ''), document.date))
+        print("{} ({})\n".format(summary, doc_no))
+        rank_doc_no[i] = doc_no
+        i += 1
+        if i > limit:
+            break
 
-o_dict = bm25.calculate_bm25(0, query, token_token_id, token_id_postings, doc_id_no, doc_no_metadata, average_length, False, DIRECTORY_PATH)
-# Display top ten results
-limit = min(len(o_dict), 10)
-i = 1
-for doc_no, score in o_dict.items():
-    document = doc_no_metadata[doc_no]
-    print("{}. {} ({})".format(i, document.headline.replace('\n', ''), document.date))
-    print(document.text)
-    summary = queryBiasedSummary.summarize(query, document.text)
-    print("{} ({})".format(summary, doc_no))
-    break
-    i += 1
-    if i > limit:
-        break
+    stop = time.time()
+    print("Retrieval took {:.3f} seconds".format(stop - start))
+    return rank_doc_no
 
-stop = time.time()
-print(stop - start)
+
+def user_query():
+    query = input("Enter a query: ")
+    prompt = ''
+
+    rankings = handle_query(query, token_token_id, token_id_postings, doc_id_no, doc_no_metadata, average_length,
+                            DIRECTORY_PATH)
+
+    while prompt not in VALID_INPUTS:
+        prompt = input("Enter document rank to view document, \'N\' for new query or \'Q\' to quit: ")
+        prompt = prompt.lower()
+        if prompt == 'q':
+            print("Quitting...")
+            exit()
+
+        if prompt == 'n':
+            user_query()
+
+        try:
+            rank = int(prompt)
+            if len(rankings) >= rank > 0:
+                doc_no = rankings[rank]
+                print(getDocument.retrieve_by_docno(DIRECTORY_PATH, doc_no).text)
+            else:
+                print("Invalid response, try again.")
+        except ValueError:
+            print("Invalid response, try again.")
+            pass
+
+
+if __name__ == "__main__":
+    invalid_input = True
+    # Import relevant dicts of tokens to postings
+    print("Importing relevant dicts")
+    token_id_postings = pickle.load(open(DIRECTORY_PATH + TOKEN_ID_POSTINGS_PATH, 'rb'))
+    token_id_token = pickle.load(open(DIRECTORY_PATH + TOKEN_ID_TOKEN_PATH, 'rb'))
+    token_token_id = pickle.load(open(DIRECTORY_PATH + TOKEN_TOKEN_ID_PATH, 'rb'))
+    doc_id_no = pickle.load(open(DIRECTORY_PATH + DOC_ID_NO_PATH, 'rb'))
+    doc_no_metadata = pickle.load(open(DIRECTORY_PATH + DOC_NO_METADATA_PATH, 'rb'))
+    print("Completed import")
+
+    # Calculate average doc length in collection
+
+    average_length = sum([doc_no_metadata[doc_no].length for doc_id, doc_no in doc_id_no.items()]) / len(doc_id_no)
+
+    user_query()
